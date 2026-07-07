556bbf4b7a6159b9d2a285e9f678f1d7416e8ff1
LoukmaneHS <loukmanehadjsaid210@gmail.com>
2026-07-07 11:04:02 +0100

    Import encode_image and decode_image functions

--- __init__.py
+++ __init__.py
@@ -2,3 +2,5 @@ from .core.classes.karacter import Karacter
 from .core.classes.kinematrix import Kinematrix
 from .core.classes.kimage import Kimage
 from .core.functions.motion import create_motion
+from .core.functions.image import encode_image
+from .core.functions.image import decode_image
