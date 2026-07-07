f666a4b3b5e61e82c98553c7754a9af03155aa8e
LoukmaneHS <loukmanehadjsaid210@gmail.com>
2026-07-07 09:45:20 +0100

    Change accumulator type from uint16 to uint32

--- core/karacter.py
+++ core/karacter.py
@@ -7,7 +7,7 @@ class Karacter:
         
         self.model_name = name
         self.dof_count = np.uint16(dof)
-        self.asa = np.zeros(dof, dtype=np.uint16)
+        self.accumulator = np.zeros(dof, dtype=np.uint32)
 
     def __repr__(self):
         return f"Karacter(name={self.model_name!r}, dof={self.dof_count})"
