73d0be99037d0d54f8ba44dc912fc639845ca6ca
LoukmaneHS <loukmanehadjsaid210@gmail.com>
2026-07-07 17:25:41 +0100

    Rename update_accumulator to acc_update

--- core/functions/character.py
+++ core/functions/character.py
@@ -3,7 +3,7 @@ from ..classes.karacter import Karacter
 from ..classes.kframe import Kframe
 
 
-def update_accumulator(karacter: Karacter, kframe: Kframe) -> None:
+def acc_update(karacter: Karacter, kframe: Kframe) -> None:
     if kframe.shape[0] != int(karacter.dof_count):
         raise ValueError(
             f"kframe dof ({kframe.shape[0]}) does not match "
