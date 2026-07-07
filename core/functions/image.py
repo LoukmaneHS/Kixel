68e1254f30cccb9a65b04a496cc2d1b5179cbf04
LoukmaneHS <loukmanehadjsaid210@gmail.com>
2026-07-07 11:25:49 +0100

    Refactor encode/decode functions to use Kmatrix

--- core/functions/image.py
+++ core/functions/image.py
@@ -1,12 +1,12 @@
 import numpy as np
-from ..classes.kinematrix import Kinematrix
+from ..classes.kmatrix import Kmatrix
 from ..classes.kimage import Kimage
 
 
-def encode_image(kinematrix: Kinematrix) -> Kimage:
-    img = Kimage(row=int(kinematrix.row), column=int(kinematrix.column))
+def encode_image(kmatrix: Kmatrix) -> Kimage:
+    img = Kimage(row=int(kmatrix.row), column=int(kmatrix.column))
 
-    raw = kinematrix.kinematrix.astype('>i4')
+    raw = kmatrix.kmatrix.astype('>i4')
     byte_view = raw.view(np.uint8).reshape(raw.shape[0], raw.shape[1], 4)
 
     img.kimage[:] = byte_view
@@ -14,13 +14,13 @@ def encode_image(kinematrix: Kinematrix) -> Kimage:
     return img
 
 
-def decode_image(kimage: Kimage) -> Kinematrix:
+def decode_image(kimage: Kimage) -> Kmatrix:
     row = int(kimage.row)
     column = int(kimage.column)
 
     raw = kimage.kimage.reshape(row, column, 4).view('>i4').reshape(row, column)
 
-    mat = Kinematrix(row=row, column=column)
-    mat.kinematrix[:] = raw.astype(np.int32)
+    mat = Kmatrix(row=row, column=column)
+    mat.kmatrix[:] = raw.astype(np.int32)
 
     return mat
