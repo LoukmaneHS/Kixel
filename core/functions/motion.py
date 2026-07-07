204cf249cccc7c28ae3673c8af037b60046aa144
LoukmaneHS <loukmanehadjsaid210@gmail.com>
2026-07-07 11:26:25 +0100

    Replace Kinematrix with Kmatrix in create_motion

--- core/functions/motion.py
+++ core/functions/motion.py
@@ -1,6 +1,6 @@
 from ..classes.karacter import Karacter
-from ..classes.kinematrix import Kinematrix
+from ..classes.kmatrix import Kmatrix
 
 
-def create_motion(karacter: Karacter, frames_number: int) -> Kinematrix:
-    return Kinematrix(column=int(karacter.dof_count), row=frames_number)
+def create_motion(karacter: Karacter, frames_number: int) -> Kmatrix:
+    return Kmatrix(column=int(karacter.dof_count), row=frames_number)
