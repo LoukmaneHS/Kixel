f07fa1bd25b33c087dc394e050886baf18a79762
LoukmaneHS <loukmanehadjsaid210@gmail.com>
2026-07-09 06:35:59 +0100

    Delete core/classes/Ksign.py

--- core/classes/Ksign.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class Ksign:
-    def __init__(self, dof: int):
-        self.dof_count = np.uint16(dof)
-        self.values = np.zeros(dof, dtype=bool)
-
-    def __repr__(self):
-        return f"Ksign(dof={self.dof_count})"
