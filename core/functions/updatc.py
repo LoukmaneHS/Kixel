47f6bb45d52af22fd6d57250acaa6870e2d1195a
LoukmaneHS <loukmanehadjsaid210@gmail.com>
2026-07-10 04:43:28 +0100

    Delete core/functions/updatc.py

--- core/functions/updatc.py
+++
@@ -1,9 +0,0 @@
-from ..classes.karacter import Karacter
-from ..classes.kframe import Kframe
-from .update import update
-from .clear import clear
-
-
-def updatc(karacter: Karacter, kframe: Kframe) -> None:
-    update(karacter, kframe)
-    clear(kframe)
