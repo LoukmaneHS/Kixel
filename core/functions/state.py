6fbcd122240f782345d50cf54467cfedfc297cfe
LoukmaneHS <loukmanehadjsaid210@gmail.com>
2026-07-12 11:06:55 +0100

    Delete core/functions/state.py

--- core/functions/state.py
+++
@@ -1,6 +0,0 @@
-def state(karacter: Karacter) -> dict[str, np.ndarray]:
-    return {
-        "nacc": karacter.nacc.copy(),
-        "oacc": karacter.oacc.copy(),
-        "sacc": karacter.sacc.copy(),
-    }
